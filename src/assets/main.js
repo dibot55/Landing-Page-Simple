7a12abb8e803b7b278aa5b98cc40f4f0e4b46b04
dibot55 <diebots555@gmail.com>
2023-07-21 12:38:26 -0600

    Vistas del consumo de la API

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,6 @@
 // API de mi canal de youtube
 const url = 'https://youtube-v31.p.rapidapi.com/captions?part=snippet&videoId=M7FIvfx5J10';
+const content = null || document.getElementById('content');
 const options = {
 	method: 'GET',
 	headers: {
@@ -22,7 +23,23 @@ async function fetchData(urlApi){
 (async () => {
   try {
     const videos = await fetchData(url);
-    return videos;
+    let view = `
+    ${videos.items.map(video => `
+      <div class="group relative">
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden          group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+          </h3>
+        </div>
+      </div>
+      `).slice(0,4).join('')}
+    `;
+    content.innerHTML = view;
   } catch (error) {
     console.log(error);
   }
